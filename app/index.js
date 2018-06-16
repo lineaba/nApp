9ccee904ec8f396fd5e4494c1cb439ba3e33c639
Fion Chan <fion.sun@gmail.com>
2018-06-16 15:13:02 -0700

    deleted index.js

--- app/index.js
+++
@@ -1,8 +0,0 @@
-import clock from "clock";
-import document from "document";
-import { vibration } from "haptics";
-import { me } from "appbit";
-import * as CDTimer from "../common/CountdownTimer.js";
-
-CDTimer.setNapTime('1');
-setInterval(() => {console.log("1 minute has passed.")}, 60000) ;
