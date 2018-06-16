9efd3d2c1dc633d2e768107c1020dc6d9af56469
Fion Chan <fion.sun@gmail.com>
2018-06-16 11:47:37 -0700

    Countdown timer on clockface ; user input for napTime to be set

---
+++ common/CountdownTimer.js
@@ -0,0 +1,45 @@
+import clock from "clock";
+import document from "document";
+import * as util from "../common/utils";
+import * as time from "../common/time";
+
+
+// napTime to be user input
+const napTime = '1';
+
+// Get a handle on the <text> element
+const myLabel = document.getElementById("myLabel");
+
+// set naptTime & allow user to start timer with button
+const setNapTime = function(napTime) {
+  this.napTime = napTime();
+  // update
+  myLabel.text = `${this.napTime}`;
+  // CONTROL BUTTON 
+  document.onkeypress = function(e) {
+    console.log("Key pressed: " + e.key); //just prints to log that button has been pressed
+    startCountdownTimer();
+  }
+}
+
+// start countdown timer
+const startCountdownTimer = function() {
+  napTime++;
+  // updates the clock every minute
+  clock.granularity = "minutes";
+  // start ticking (per minute) the clock
+  clock.ontick = (evt) => {
+    napTime--;
+    // stop ticking clock & ring alarm
+    if (napTime === 0) {
+      clock.granularity = "off";
+      vibration.start("ring");
+    }
+    updateTimer(napTime);
+  }
+}
+
+// update minutes left on clock face
+const updateTimer = (minsLeft) => {
+    myLabel.text = `${minsLeft}`;
+}
